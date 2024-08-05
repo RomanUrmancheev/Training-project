6e18f9f1481c9dddcc2ca8146cad4bf3d4680bb1
Roman Urmancheev <roman.urmancheev4@gmail.com>
2024-08-05 19:54:41 +0300

    docker settings added

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
 const chalk = require("chalk");
+const path = require("path");
 const initDatabase = require("./startUp/initDatabase");
 const routes = require("./routes");
 const cors = require("cors");
@@ -14,6 +15,16 @@ app.use(cors());
 app.use("/api", routes);
 
 const PORT = config.get("port") ?? 8080;
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client")));
+
+  const indexPath = path.join(__dirname, "client", "index.html");
+
+  app.get("*", (req, res) => {
+    res.sendFile(indexPath);
+  });
+}
 
 const start = async () => {
   try {
